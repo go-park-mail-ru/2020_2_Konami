0158007d095c7f0c72ab9cad0f52d550beac0d53
Dmitry <dimkashow27@mail.ru>
2020-11-30 22:10:26 +0300

    add FULL TEST csrf - 7.1% overall

---
+++ internal/pkg/middleware/test_utils.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+)
+
+type RouteArgs struct {
+	Key   string
+	Value string
+}
+
+func SetMuxVars(next http.HandlerFunc, args []RouteArgs) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		for _, val := range args {
+			ctx = context.WithValue(ctx, val.Key, val.Value)
+		}
+		next(w, r.WithContext(ctx))
+	}
+}
