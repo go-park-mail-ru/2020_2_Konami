1ecadb1800e724ca0f6fa8aa6db09e43a7be7ef1
Dmitry <dimkashow27@mail.ru>
2020-11-30 23:16:35 +0300

    add FULL TEST - 100%%%% tag - 11% overall

--- internal/pkg/tag/repository.go
+++ internal/pkg/tag/repository.go
@@ -1,3 +1,4 @@
+//go:generate mockgen -source=repository.go -destination=./repositoty_mock.go -package=tag
 package tag
 
 import (
--- internal/pkg/tag/usecase.go
+++ internal/pkg/tag/usecase.go
@@ -1,3 +1,4 @@
+//go:generate mockgen -source=usecase.go -destination=./usecase_mock.go -package=tag
 package tag
 
 import (
